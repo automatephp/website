473fd4bf3141869a519bdc4c08d7a44105281303
Julien Jacottet <jjacottet@gmail.com>
2020-02-23 18:07:17 +0100

    update min php version

--- static/installer.php
+++ static/installer.php
@@ -33,10 +33,10 @@ namespace
 
     // check version
     check(
-        'You have a supported version of PHP (>= 5.6).',
+        'You have a supported version of PHP (>= 7.2.5).',
         'You need PHP 5.6 or greater.',
         function () {
-            return version_compare(PHP_VERSION, '5.6', '>=');
+            return version_compare(PHP_VERSION, '7.2.5', '>=');
         }
     );
 
